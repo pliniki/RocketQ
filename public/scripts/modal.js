89de934cb790557b5bf37c586cedb9e80fe3d53a
Paulo Carvalho <93048633+pliniki@users.noreply.github.com>
2021-12-20 14:17:36 -0300

    Atualizacao do projeto final

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,11 +1,17 @@
 export default function Modal() {
+  const modalWrapper = document.querySelector('.modal-wrapper')
+  const cancelButton = document.querySelector('.button.cancel')
+
+  cancelButton.addEventListener('click', close)
+
   function open() {
     //Funcionalidade de atribuir a class "active" para a modal
-    document.querySelector('.modal-wrapper').classList.add('active')
+    modalWrapper.classList.add('active')
   }
 
   function close() {
     //Funcionalidade de remover a class "active" da modal
+    modalWrapper.classList.remove('active')
   }
 
   return {
